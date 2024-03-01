53d71664db544085d7d5acb641e446efe0a8e5b4
serkanisyapan <serkanisyapan1@hotmail.com>
2024-03-01 13:57:41 +0300

    more accurate wpm calc

--- src/components/InputContainer.jsx
+++ src/components/InputContainer.jsx
@@ -1,22 +1,31 @@
-import { Timer } from "./Timer"
+import { Timer } from "./Timer";
 
-export const InputContainer = ({focusRef, userInput, wrongLetters, handleKeyDown, handleOnChange, gameMode, gameState, keyStrokes }) => {
+export const InputContainer = ({
+  focusRef,
+  userInput,
+  wrongLetters,
+  handleKeyDown,
+  handleOnChange,
+  gameMode,
+  gameState,
+  keyStrokes,
+}) => {
   return (
-      <div className="input-container">
-        <input
-          ref={focusRef}
-          className="word-input"
-          value={userInput}
-          onKeyDown={handleKeyDown}
-          onChange={handleOnChange}
-          disabled={gameState === "runIsOver" ? true : false}
-        />
-        <Timer
-          gameMode={gameMode}
-          keyStrokes={keyStrokes}
-          gameState={gameState}
-          wrongLetters={wrongLetters}
-        />
-      </div>
-  )
-}
+    <div className="input-container">
+      <input
+        ref={focusRef}
+        className="word-input"
+        value={userInput}
+        onKeyDown={handleKeyDown}
+        onChange={handleOnChange}
+        disabled={gameState === "runIsOver" ? true : false}
+      />
+      <Timer
+        gameMode={gameMode}
+        keyStrokes={keyStrokes}
+        gameState={gameState}
+        wrongLetters={wrongLetters}
+      />
+    </div>
+  );
+};
